c68421afe72744f15e3048ab396e88035c279aa2
maxmorev <maxmorev@gmail.com>
2019-05-30 14:33:30 +0300

    Adding Validation layer tested: method to add commodity

--- src/test/java/ru/maxmorev/restful/eshop/TestSpeedAlgorithm.java
+++ src/test/java/ru/maxmorev/restful/eshop/TestSpeedAlgorithm.java
@@ -10,20 +10,18 @@ public class TestSpeedAlgorithm {
         Random random = new Random();
         List<Long> values = new ArrayList<Long>(10000000);
         values.forEach(v->values.add(random.nextLong()));
-        Set<Long> valuesSet = new HashSet<>(values);
         List<Long> branch = new ArrayList<>(10000000);
         values.forEach(v->branch.add(v));
         boolean eq;
         long start = new Date().getTime();
         Collections.sort(values);
-        if(valuesSet.size()==branch.size()){
+        if(values.size()==branch.size()){
             //check values
-            /*Set<Long> sortedValsSet = new TreeSet<>();//u can try to set TreeSet instead HashSet
+            Set<Long> sortedValsSet = new HashSet<>();//u can try to set TreeSet instead HashSet
             branch.forEach(propertySet->sortedValsSet.add(propertySet));
-            */
             eq = true;
-            for(Long v:branch){
-                if(valuesSet.add(v)){
+            for(Long v:values){
+                if(sortedValsSet.add(v)){
                     eq = false;
                     break;
                 }
